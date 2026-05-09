cc09e92506ea826e5eae5167f2dfa24cd398e8dc
milena579 <milecalegari@gmail.com>
2026-05-08 22:52:20 -0300

    feat: configurando github action

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ds881-curriculo-GRR20245575/'
 })
